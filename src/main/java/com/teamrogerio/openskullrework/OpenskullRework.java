c48f82947794442638fb19fa5f23a0f64e804444
Lucas da Silva dos Santos <lucassilva.lds@outlook.com>
2020-03-30 19:15:42 -0300

    First Commit

---
+++ src/main/java/com/teamrogerio/openskullrework/OpenskullRework.java
@@ -0,0 +1,13 @@
+package com.teamrogerio.openskullrework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenskullRework {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenskullRework.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamrogerio/openskullrework/OpenskullreworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamrogerio.openskullrework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenskullreworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
