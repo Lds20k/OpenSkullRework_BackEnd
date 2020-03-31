dbad027b002c1978bea3de69e17a2e26aa7e9bdf
Bruno Ventura <brunoventura80@gmail.com>
2020-03-31 10:51:06 -0300

    DB domain person model created

---
+++ src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/model/PersonDBDomain.java
@@ -0,0 +1,28 @@
+package com.teamrogerio.openskullrework.gateway.mongodb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+
+@Document(collection = "person")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDBDomain {
+    public static final String AMERICA_SAO_PAULO = "America/Sao_Paulo";
+    @Id
+    private String id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+    private ZoneId zonedId = ZoneId.of(AMERICA_SAO_PAULO);
+    private Instant dataDeCriacao = LocalDateTime.now().toInstant(OffsetDateTime.now().toLocalDateTime().atZone(zonedId).getOffset());
+}
