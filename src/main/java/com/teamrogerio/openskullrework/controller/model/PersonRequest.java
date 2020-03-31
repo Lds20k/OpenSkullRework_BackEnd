d9881a3922241d82a889365594d6c52b7a6f68eb
Bruno Ventura <brunoventura80@gmail.com>
2020-03-31 10:51:31 -0300

    PersonRequest model created

---
+++ src/main/java/com/teamrogerio/openskullrework/controller/model/PersonRequest.java
@@ -0,0 +1,16 @@
+package com.teamrogerio.openskullrework.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PersonRequest {
+
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+}
