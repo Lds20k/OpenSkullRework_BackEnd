5675fcf76a26b218a2fcacbec4f0a400c35e258c
Bruno Ventura <brunoventura80@gmail.com>
2020-04-06 17:53:56 -0300

    Image response model created

---
+++ src/main/java/com/teamrogerio/openskullrework/controller/model/ImageResponse.java
@@ -0,0 +1,12 @@
+package com.teamrogerio.openskullrework.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ImageResponse {
+    private String base64;
+}
