956af3f1e95435269588b8da9e80a3e7bd943d30
Bruno Ventura <brunoventura80@gmail.com>
2020-04-06 17:58:15 -0300

    Get person image in base64 use case created

---
+++ src/main/java/com/teamrogerio/openskullrework/usecase/GetPersonImageUseCase.java
@@ -0,0 +1,32 @@
+package com.teamrogerio.openskullrework.usecase;
+
+import com.teamrogerio.openskullrework.controller.exception.PersonDoesNotExistsException;
+import com.teamrogerio.openskullrework.controller.model.ImageResponse;
+import com.teamrogerio.openskullrework.gateway.mongodb.model.PersonDBDomain;
+import com.teamrogerio.openskullrework.gateway.mongodb.repository.FindPersonByIdGateway;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class GetPersonImageUseCase {
+
+    private final FindPersonByIdGateway findPersonByIdGateway;
+
+    public ImageResponse execute(String personId) throws PersonDoesNotExistsException, IOException {
+        Optional<PersonDBDomain> personDBDomainOptional = findPersonByIdGateway.execute(personId);
+        if (!personDBDomainOptional.isPresent()) throw new PersonDoesNotExistsException("Person does not exists");
+
+        File file = new File(new File("public\\images\\user\\" + personDBDomainOptional.get().getImage()).getAbsolutePath());
+
+        byte[] bytes = new byte[(int) file.length()];
+        new FileInputStream(file).read(bytes);
+        return new ImageResponse(new String(Base64.encodeBase64(bytes)));
+    }
+}
