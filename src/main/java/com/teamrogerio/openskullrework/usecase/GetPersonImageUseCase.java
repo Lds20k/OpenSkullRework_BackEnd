8065abdaccc3a2ab672d78f4fc5c85c6d5654788
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 15:25:32 -0300

    Code improvement

--- src/main/java/com/teamrogerio/openskullrework/usecase/GetPersonImageUseCase.java
+++ src/main/java/com/teamrogerio/openskullrework/usecase/GetPersonImageUseCase.java
@@ -3,7 +3,7 @@ package com.teamrogerio.openskullrework.usecase;
 import com.teamrogerio.openskullrework.controller.exception.PersonDoesNotExistsException;
 import com.teamrogerio.openskullrework.controller.model.ImageResponse;
 import com.teamrogerio.openskullrework.gateway.mongodb.model.PersonDBDomain;
-import com.teamrogerio.openskullrework.gateway.mongodb.repository.FindPersonByIdGateway;
+import com.teamrogerio.openskullrework.gateway.mongodb.repository.GetPersonByIdGateway;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
@@ -11,20 +11,18 @@ import org.springframework.stereotype.Component;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
 public class GetPersonImageUseCase {
 
-    private final FindPersonByIdGateway findPersonByIdGateway;
+    private final GetPersonByIdGateway getPersonByIdGateway;
 
     public ImageResponse execute(String personId) throws PersonDoesNotExistsException, IOException {
-        Optional<PersonDBDomain> personDBDomainOptional = findPersonByIdGateway.execute(personId);
-        if (!personDBDomainOptional.isPresent()) throw new PersonDoesNotExistsException("Person does not exists");
+        PersonDBDomain personDBDomain = getPersonByIdGateway.execute(personId);
         File file;
-        if (personDBDomainOptional.get().getImage() != null) {
-            file = new File(new File("public\\images\\user\\" + personDBDomainOptional.get().getImage()).getAbsolutePath());
+        if (personDBDomain.getImage() != null) {
+            file = new File(new File("public\\images\\user\\" + personDBDomain.getImage()).getAbsolutePath());
         } else {
             file = new File(new File("public\\images\\user\\default.png").getAbsolutePath());
         }
