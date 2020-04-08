7965d7b07ebb7eb36ac5b40ca42b15922fb42ce2
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 15:34:31 -0300

    AddCourseIntoPersonUseCase modified

--- src/main/java/com/teamrogerio/openskullrework/usecase/AddCourseIntoPersonUseCase.java
+++ src/main/java/com/teamrogerio/openskullrework/usecase/AddCourseIntoPersonUseCase.java
@@ -1,11 +1,15 @@
 package com.teamrogerio.openskullrework.usecase;
 
 import com.teamrogerio.openskullrework.controller.exception.CourseDoesNotExistsException;
-import com.teamrogerio.openskullrework.controller.exception.PersonAlreadyHaveCourseException;
 import com.teamrogerio.openskullrework.controller.exception.PersonDoesNotExistsException;
-import com.teamrogerio.openskullrework.controller.model.PersonResponse;
+import com.teamrogerio.openskullrework.controller.model.EnrollmentResponse;
 import com.teamrogerio.openskullrework.controller.translator.Translator;
+import com.teamrogerio.openskullrework.entities.Course;
+import com.teamrogerio.openskullrework.entities.Enrollment;
+import com.teamrogerio.openskullrework.entities.Person;
 import com.teamrogerio.openskullrework.gateway.mongodb.repository.AddCourseIntoPersonGateway;
+import com.teamrogerio.openskullrework.gateway.mongodb.repository.GetCourseByIdGateway;
+import com.teamrogerio.openskullrework.gateway.mongodb.repository.GetPersonByIdGateway;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +18,16 @@ import org.springframework.stereotype.Component;
 public class AddCourseIntoPersonUseCase {
 
     private final AddCourseIntoPersonGateway addCourseIntoPersonGateway;
+    private final GetCourseByIdGateway getCourseByIdGateway;
+    private final GetPersonByIdGateway getPersonByIdGateway;
 
-    public PersonResponse execute(String personId, String courseId) throws PersonAlreadyHaveCourseException, PersonDoesNotExistsException, CourseDoesNotExistsException {
-        return Translator.translate(addCourseIntoPersonGateway.execute(personId, courseId), PersonResponse.class);
+    public EnrollmentResponse execute(String personId, String courseId) throws CourseDoesNotExistsException, PersonDoesNotExistsException {
+        Course course = Translator.translate(getCourseByIdGateway.execute(courseId), Course.class);
+        Person person = Translator.translate(getPersonByIdGateway.execute(personId), Person.class);
+
+        Enrollment enrollment = new Enrollment();
+        enrollment.setCourse(course);
+        enrollment.setPerson(person);
+        return Translator.translate(addCourseIntoPersonGateway.execute(enrollment), EnrollmentResponse.class);
     }
 }
