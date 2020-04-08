114b5098519a9a4442bbab25b4673e3ea9abaab8
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 15:29:54 -0300

    Files removed

--- src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/repository/FindPersonByIdGateway.java
+++
@@ -1,9 +0,0 @@
-package com.teamrogerio.openskullrework.gateway.mongodb.repository;
-
-import com.teamrogerio.openskullrework.gateway.mongodb.model.PersonDBDomain;
-
-import java.util.Optional;
-
-public interface FindPersonByIdGateway {
-    Optional<PersonDBDomain> execute(String id);
-}
