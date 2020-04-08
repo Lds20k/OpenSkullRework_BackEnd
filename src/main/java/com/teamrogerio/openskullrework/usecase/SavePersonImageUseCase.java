d3a404c10b41f92aaf9dd45f051a71acb6eac9be
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 15:24:17 -0300

    Code improvement

--- src/main/java/com/teamrogerio/openskullrework/usecase/SavePersonImageUseCase.java
+++ src/main/java/com/teamrogerio/openskullrework/usecase/SavePersonImageUseCase.java
@@ -7,7 +7,7 @@ import com.teamrogerio.openskullrework.controller.model.PersonResponse;
 import com.teamrogerio.openskullrework.controller.translator.Translator;
 import com.teamrogerio.openskullrework.entities.Person;
 import com.teamrogerio.openskullrework.gateway.mongodb.model.PersonDBDomain;
-import com.teamrogerio.openskullrework.gateway.mongodb.repository.FindPersonByIdGateway;
+import com.teamrogerio.openskullrework.gateway.mongodb.repository.GetPersonByIdGateway;
 import com.teamrogerio.openskullrework.gateway.mongodb.repository.UpdatePersonGateway;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
@@ -17,30 +17,28 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
 public class SavePersonImageUseCase {
 
-    private final FindPersonByIdGateway findPersonByIdGateway;
+    private final GetPersonByIdGateway getPersonByIdGateway;
     private final UpdatePersonGateway updatePersonGateway;
     private final VerifyFileCompatibilityUseCase verifyFileCompatibilityUseCase;
 
     public PersonResponse execute(String personId, MultipartFile image) throws PersonDoesNotExistsException, ProblemsToUploadImageException, FileIsNotCompatibleException {
-        Optional<PersonDBDomain> personDBDomainOptional = findPersonByIdGateway.execute(personId);
-        if (!personDBDomainOptional.isPresent()) throw new PersonDoesNotExistsException("Person does not exists");
+        PersonDBDomain personDBDomain = getPersonByIdGateway.execute(personId);
 
         verifyFileCompatibilityUseCase.execute(image);
 
-        String imgName = (personDBDomainOptional.get().getId() + "." + FilenameUtils.getExtension(image.getOriginalFilename()));
+        String imgName = (personDBDomain.getId() + "." + FilenameUtils.getExtension(image.getOriginalFilename()));
         try {
             Files.write(Paths.get((new File("public\\images\\user\\" + imgName).getAbsolutePath())), image.getBytes());
         } catch (Exception e) {
             throw new ProblemsToUploadImageException("Problem to upload the image");
         }
-        personDBDomainOptional.get().setImage(imgName);
+        personDBDomain.setImage(imgName);
 
-        return Translator.translate(updatePersonGateway.execute(Translator.translate(personDBDomainOptional.get(), Person.class)), PersonResponse.class);
+        return Translator.translate(updatePersonGateway.execute(Translator.translate(personDBDomain, Person.class)), PersonResponse.class);
     }
 }
