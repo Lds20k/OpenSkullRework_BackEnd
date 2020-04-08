7429d92a96b7602e555c9bc18cd436d9e7454f69
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 16:02:40 -0300

    CreatorCanNotSubscribeInYourOwnCourseException created

---
+++ src/main/java/com/teamrogerio/openskullrework/controller/exception/CreatorCanNotSubscribeInYourOwnCourseException.java
@@ -0,0 +1,7 @@
+package com.teamrogerio.openskullrework.controller.exception;
+
+public class CreatorCanNotSubscribeInYourOwnCourseException extends Exception {
+    public CreatorCanNotSubscribeInYourOwnCourseException(String msg) {
+        super(msg);
+    }
+}
