15f3af59ec5e37646688bfe1b47bc450f99b6b23
Bruno Ventura <brunoventura80@gmail.com>
2020-04-08 15:26:21 -0300

    Code improvement

--- src/main/java/com/teamrogerio/openskullrework/usecase/CreateNewPersonUseCase.java
+++ src/main/java/com/teamrogerio/openskullrework/usecase/CreateNewPersonUseCase.java
@@ -2,7 +2,6 @@ package com.teamrogerio.openskullrework.usecase;
 
 import com.teamrogerio.openskullrework.controller.exception.EmailFormatIsNotValidException;
 import com.teamrogerio.openskullrework.controller.exception.PersonAlreadyExistsException;
-import com.teamrogerio.openskullrework.controller.exception.PersonDoesNotExistsException;
 import com.teamrogerio.openskullrework.controller.model.PersonResponse;
 import com.teamrogerio.openskullrework.controller.translator.Translator;
 import com.teamrogerio.openskullrework.entities.Person;
@@ -16,11 +15,11 @@ public class CreateNewPersonUseCase {
 
     private final CreateNewPersonGateway createNewPersonGateway;
     private final VerifyIfEmailIsValidUseCase verifyIfEmailIsValidUseCase;
-    private final VerifyIfPersonAlreadyExistsByEmailUseCase verifyIfPersonAlreadyExistsByEmailUseCase;
+    private final VerifyIfEmailAlreadyExistsUseCase verifyIfEmailAlreadyExistsUseCase;
 
     public PersonResponse execute(Person person) throws EmailFormatIsNotValidException, PersonAlreadyExistsException {
         verifyIfEmailIsValidUseCase.execute(person.getEmail());
-        verifyIfPersonAlreadyExistsByEmailUseCase.execute(person.getEmail());
+        verifyIfEmailAlreadyExistsUseCase.execute(person.getEmail());
         return Translator.translate(createNewPersonGateway.execute(person), PersonResponse.class);
     }
 }
