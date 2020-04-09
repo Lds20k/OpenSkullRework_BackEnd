36cbbae87d7371175a2407cddd8e9db514c3b127
Bruno Ventura <brunoventura80@gmail.com>
2020-04-09 15:06:17 -0300

    ModuleRequest model created

---
+++ src/main/java/com/teamrogerio/openskullrework/controller/model/ModuleRequest.java
@@ -0,0 +1,13 @@
+package com.teamrogerio.openskullrework.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ModuleRequest {
+    private String name;
+    private String course;
+}
