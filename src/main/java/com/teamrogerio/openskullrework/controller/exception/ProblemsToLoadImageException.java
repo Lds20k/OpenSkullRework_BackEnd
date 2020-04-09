d2dbeba475cf67cd5fc69fb11387eb98cefc84b9
Bruno Ventura <brunoventura80@gmail.com>
2020-04-09 11:51:38 -0300

    ProblemsToLoadImageException created

---
+++ src/main/java/com/teamrogerio/openskullrework/controller/exception/ProblemsToLoadImageException.java
@@ -0,0 +1,7 @@
+package com.teamrogerio.openskullrework.controller.exception;
+
+public class ProblemsToLoadImageException extends Exception {
+    public ProblemsToLoadImageException(String msg) {
+        super(msg);
+    }
+}
