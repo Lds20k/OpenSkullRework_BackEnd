fef64b3fd179733f84aa9f38562f5a91ba9214d6
Bruno Ventura <brunoventura80@gmail.com>
2020-04-09 15:05:13 -0300

    Module model created

---
+++ src/main/java/com/teamrogerio/openskullrework/entities/Module.java
@@ -0,0 +1,14 @@
+package com.teamrogerio.openskullrework.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Module {
+    private String id;
+    private String course;
+    private String name;
+}
