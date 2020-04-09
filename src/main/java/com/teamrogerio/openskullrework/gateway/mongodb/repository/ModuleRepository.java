3849339e9993f2394fddd25885a031a919d71e1a
Bruno Ventura <brunoventura80@gmail.com>
2020-04-09 16:05:26 -0300

    findAllModuleByCourse method created in ModuleRepository

--- src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/repository/ModuleRepository.java
+++ src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/repository/ModuleRepository.java
@@ -3,5 +3,8 @@ package com.teamrogerio.openskullrework.gateway.mongodb.repository;
 import com.teamrogerio.openskullrework.gateway.mongodb.model.ModuleDBDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ModuleRepository extends MongoRepository<ModuleDBDomain, String> {
+    List<ModuleDBDomain> findAllModuleByCourse(String course);
 }
