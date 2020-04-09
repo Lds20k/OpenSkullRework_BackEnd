0975e5912b8dc6fbc48a4651dec8cdc6782b1a4d
Bruno Ventura <brunoventura80@gmail.com>
2020-04-09 10:44:45 -0300

    findAllCourseByCreator created in CourseRepository

--- src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/repository/CourseRepository.java
+++ src/main/java/com/teamrogerio/openskullrework/gateway/mongodb/repository/CourseRepository.java
@@ -3,5 +3,8 @@ package com.teamrogerio.openskullrework.gateway.mongodb.repository;
 import com.teamrogerio.openskullrework.gateway.mongodb.model.CourseDBDomain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends MongoRepository<CourseDBDomain, String> {
+    List<CourseDBDomain> findAllCourseByCreator(String creator);
 }
